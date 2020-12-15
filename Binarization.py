20f899a8e48867676ff9e0acd29796ab715c8abe
ahmedx3 <ahmed.moustafaa99@gmail.com>
2020-12-15 20:11:18 +0200

    Binarization trail 2

---
+++ Binarization.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+from skimage.filters import (threshold_otsu, threshold_niblack,
+                             threshold_sauvola)
+
+def AdaptiveThresholding(img,method=0,block_size=11):
+    
+    if(method == 0):
+        outputImg = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,block_size,2)
+    elif(method == 1):
+        outputImg = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,block_size,2)
+    elif(method == 2):
+        outputImg = img > threshold_niblack(img, block_size, k=0.8)
+    elif(method == 3):
+        outputImg = img > threshold_sauvola(img, block_size)         
+    
+    return outputImg
+
+def GlobalThresholding(img):
+    
+    ret2,th2 = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+    return th2
--- main.py
+++ main.py
@@ -1,58 +1,16 @@
-import cv2 as cv
-import numpy as np
-from matplotlib import pyplot as plt
-from skimage.data import page
+import cv2
+import Binarization as binarization
+from DisplayImages import *
 from skimage import exposure
-from skimage.filters import (threshold_otsu, threshold_niblack,
-                             threshold_sauvola)
-img = cv.imread('book.PNG',0)
 
-ret,th1 = cv.threshold(img,127,255,cv.THRESH_BINARY)
-th2 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_MEAN_C,\
-            cv.THRESH_BINARY,21,2)
-th3 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,\
-            cv.THRESH_BINARY,21,2)
+originalImage = cv2.imread('Images/music2.png',cv2.IMREAD_GRAYSCALE)
 
-# th2 = cv.medianBlur(th2,3)
-# th3 = cv.medianBlur(th3,3)
+# Binarization Step
 
-titles = ['Original Image', 'Global Thresholding (v = 127)',
-            'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
-images = [img, th1, th2, th3]
-for i in range(4):
-    plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
-    plt.title(titles[i])
-    plt.xticks([]),plt.yticks([])
-plt.show()
+thre1 = binarization.AdaptiveThresholding(originalImage,0,11) # sometimes make holes in the note
+thre2 = binarization.AdaptiveThresholding(originalImage,1,11)
+thre3 = binarization.AdaptiveThresholding(originalImage,2,11)
+thre4 = binarization.AdaptiveThresholding(originalImage,3,11) # give good results
+thre5 = binarization.GlobalThresholding(originalImage)
 
-# binary_global = img > threshold_otsu(img)
-
-# window_size = 11
-# thresh_niblack = threshold_niblack(img, window_size=window_size, k=0.8)
-# thresh_sauvola = threshold_sauvola(img, window_size=window_size)
-
-# binary_niblack = img > thresh_niblack
-# binary_sauvola = img > thresh_sauvola
-
-# plt.figure(figsize=(8, 7))
-# plt.subplot(2, 2, 1)
-# plt.imshow(img, cmap=plt.cm.gray)
-# plt.title('Original')
-# plt.axis('off')
-
-# plt.subplot(2, 2, 2)
-# plt.title('Global Threshold')
-# plt.imshow(binary_global, cmap=plt.cm.gray)
-# plt.axis('off')
-
-# plt.subplot(2, 2, 3)
-# plt.imshow(binary_niblack, cmap=plt.cm.gray)
-# plt.title('Niblack Threshold')
-# plt.axis('off')
-
-# plt.subplot(2, 2, 4)
-# plt.imshow(binary_sauvola, cmap=plt.cm.gray)
-# plt.title('Sauvola Threshold')
-# plt.axis('off')
-
-# plt.show()
+show_images([thre1,thre2,thre3,thre4,thre5],['ADAPTIVE_MEAN','ADAPTIVE_GAUSSIAN','niblack','sauvola','Otsu'])
