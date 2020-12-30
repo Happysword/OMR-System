a367fe132f05bc12af7795da7ac9b942f1bab48d
3omar-mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2020-12-30 16:19:47 +0200

    Added handling command line arguments

--- main.py
+++ main.py
@@ -6,36 +6,44 @@ from fix_orientation import fix_orientation
 from staff import *
 from segment import *
 from NotesDetection import *
+import io_utils
 
-originalImage = cv2.imread('Images/music3.png',cv2.IMREAD_GRAYSCALE)
+args = io_utils.get_command_line_args()
+__DEBUG__ = args.debug
 
-# Binarization Step
+for filename in io_utils.get_filenames(args.input_path):
+    try:  # Use to ignore any error and go to next file instead of exiting
 
-thre4 = binarization.AdaptiveThresholding(originalImage,3,21) # give good results
+        originalImage = io_utils.read_grayscale_image(args.input_path, filename)
 
-# Fixing Orientation Step (Fixing Rotation and Perspective and Crop)
-fixed_orientation = fix_orientation(thre4)
-# show_images([fixed_orientation])
+        # Binarization Step
+        thre4 = binarization.AdaptiveThresholding(originalImage, 3, 21)  # give good results
 
-segmented_staffs_array = segment_staff(thre4*255)
+        # Fixing Orientation Step (Fixing Rotation and Perspective and Crop)
+        fixed_orientation = fix_orientation(thre4, __DEBUG__)
+        # show_images([fixed_orientation])
 
-# Getting Staff features
+        # utils.write_image(fixed_orientation, args.output_path, filename)
 
-Staffs = []
-for segment in segmented_staffs_array:
-    Staffs.append(Staff(np.uint8(segment)))
-for i in Staffs:
-    show_images([i.lines,i.notes],["Detected Lines", "Detected notes"])
+        segmented_staffs_array = segment_staff(thre4 * 255)
 
-symbols = []      
-for staff in Staffs:
-    temp = segment_symbols(staff.notes)
-    symbols = symbols + temp
-    show_images(temp)
-    # print(segment_symbols(staff.notes))
-    # print(staff.positions)
-    # notePoints,notesNames = NotesPositions(thre4,staff.positions,staff.space)
-    # print(notesNames)
+        # Getting Staff features
 
+        staffs = []
+        for segment in segmented_staffs_array:
+            staffs.append(Staff(np.uint8(segment)))
+        for i in staffs:
+            show_images([i.lines, i.notes], ["Detected Lines", "Detected notes"])
 
-
+        symbols = []
+        for staff in staffs:
+            temp = segment_symbols(staff.notes)
+            symbols = symbols + temp
+            show_images(temp)
+            # print(segment_symbols(staff.notes))
+            # print(staff.positions)
+            # notePoints,notesNames = NotesPositions(thre4,staff.positions,staff.space)
+            # print(notesNames)
+    except Exception as e:
+        print(e)
+        pass
