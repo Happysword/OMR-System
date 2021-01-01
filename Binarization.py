38959eed46a5704d1dee9dc805ed2c4474767937
ahmedx3 <ahmed.moustafaa99@gmail.com>
2021-01-01 23:13:40 +0200

    Fixed binarization

--- Binarization.py
+++ Binarization.py
@@ -3,16 +3,25 @@ import numpy as np
 from skimage.filters import (threshold_otsu, threshold_niblack,
                              threshold_sauvola)
 
-def AdaptiveThresholding(img,method=0,block_size=11):
+def AdaptiveThresholding(img,method=0):
+
+    w,h = img.shape[:2]
+    blockSize = max(w,h)
+    blockSize = int(blockSize *0.02)
     
+    if blockSize % 2 == 0:
+        blockSize += 1
+
+    print("blockSize " + str(blockSize))
+
     if(method == 0):
-        outputImg = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,block_size,8)
+        outputImg = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,blockSize,8)
     elif(method == 1):
-        outputImg = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,block_size,8)
+        outputImg = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,blockSize,8)
     elif(method == 2):
-        outputImg = img > threshold_niblack(img, block_size, k=0.8)
+        outputImg = img > threshold_niblack(img, blockSize, k=0.8)
     elif(method == 3):
-        outputImg = img > threshold_sauvola(img, block_size) 
+        outputImg = img > threshold_sauvola(img, blockSize) 
     
     return outputImg
 
--- main.py
+++ main.py
@@ -17,29 +17,29 @@ for filename in io_utils.get_filenames(args.input_path):
         originalImage = io_utils.read_grayscale_image(args.input_path, filename)
 
         # Binarization Step
-        thre4 = binarization.AdaptiveThresholding(originalImage, 3, 21)  # give good results
+        thre4 = binarization.AdaptiveThresholding(originalImage, 3)  # give good results
 
         # Fixing Orientation Step (Fixing Rotation and Perspective and Crop)
         fixed_orientation = fix_orientation(thre4, __DEBUG__)
-        # show_images([fixed_orientation])
+        show_images([fixed_orientation])
 
         # utils.write_image(fixed_orientation, args.output_path, filename)
 
         segmented_staffs_array = segment_staff(fixed_orientation)
-        # show_images(segmented_staffs_array)
+        show_images(segmented_staffs_array)
         # # # Getting Staff features
 
         staffs = []
         for segment in segmented_staffs_array:
             staffs.append(Staff(np.uint8(segment)))
-        # for i in staffs:
-        #     show_images([i.lines, i.notes], ["Detected Lines", "Detected notes"])
+        for i in staffs:
+            show_images([i.lines, i.notes], ["Detected Lines", "Detected notes"])
 
         symbols = []
         for staff in staffs:
             temp = segment_symbols(staff.notes)
             symbols = symbols + temp
-            # show_images(temp)
+            show_images(temp)
             # print(segment_symbols(staff.notes))
             # print(staff.positions)
             notePoints,notesNames = NotesPositions(staff.image,staff.positions,staff.space,staff.notes)
