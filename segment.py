39225118c68fc64023ffe01c96bc8f102e7ef2f6
3omar-mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2021-01-09 02:11:55 +0200

    Fixed bug in symbol segmentation (Merging overlapping symbols skipped last symbol)

--- segment.py
+++ segment.py
@@ -9,14 +9,14 @@ def segment_staff(img):
     #Takes a Uint8 grey image and returns an array of images of staffs
     #Only works on Non-rotated images
     inverted_img = (255 - img) / 255
-    
+
     #Dilate the image
-    
+
     struct_element = cv.getStructuringElement(cv.MORPH_RECT,(80,6)) #Should check for better structuring elements
     dilated_img = cv.dilate(inverted_img,struct_element,iterations=5)
     # debug_show_images([dilated_img]) #print dilated image
 
-    #Calculate the Horizontal histogram 
+    #Calculate the Horizontal histogram
     hist_hor = np.sum(dilated_img,axis=1)
     # plt.plot(hist_hor)
     # plt.hlines(threshold,-50,len(hist_hor)+50,colors="0.5",linestyles="dashed")
@@ -29,7 +29,7 @@ def segment_staff(img):
     percentage_taken = 0.03
     iteration_values = []
     for iteration in range(1,31) :
-        
+
         #Calculate the Threshold with a different ratio everytime
         current_max_len = int( len(sorted_hist_hor) * percentage_taken * iteration)
         averaged_partition = sorted_hist_hor[0:current_max_len] # Percentage Average Taken
@@ -48,17 +48,17 @@ def segment_staff(img):
             elif less_flag == 2 and hist_hor[i] < threshold:
                 staff_width.append( i-start_cut )
                 less_flag = 0
-        
+
         # If there was atleast one segment we add them to be compared later 
         if len(staff_width) > 0:
             sigma = np.std(staff_width)
             average_width = np.average(staff_width)
             iteration_values.append( (sigma,average_width,len(staff_width),threshold) )
-    
+
     #Criteria for a good minimum sigma
     all_sigmas = [itr[0]for itr in iteration_values]
     min_sigma = 3 if np.median(all_sigmas) <= 3 else np.median(all_sigmas)
-    
+
 
     #Initial value and base case if no segments found
     best_iteration = (0,0,0,0)
@@ -90,7 +90,7 @@ def segment_staff(img):
         elif less_flag == 2 and hist_hor[i] < threshold:
             staff_indices.append( (start_cut,i) )
             less_flag = 0
-    
+
     #Segment the Optimal Images and return them
     segmented_staffs = []
     for i in staff_indices:
@@ -105,10 +105,10 @@ def segment_symbols(img, width = 16, height = 32):
 
     #Find the Contours and and sort them According to their x values
     contours, hierarchy = cv.findContours(img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
-    boundingBoxes = [cv.boundingRect(c) for c in contours] 
+    boundingBoxes = [cv.boundingRect(c) for c in contours]
     (sorted_contours,sorted_bounding_rect) = zip(*sorted(zip(contours, boundingBoxes),key=lambda b:b[1][0]))
 
-    
+
     #Use the sorted_bounding_rect to merge the overlapping Symbols
     new_sorted_bounding_rect = []
     i = 0
@@ -117,8 +117,8 @@ def segment_symbols(img, width = 16, height = 32):
         x_max = sorted_bounding_rect[i][0] + sorted_bounding_rect[i][2]
         y_min = sorted_bounding_rect[i][1]
         y_max = sorted_bounding_rect[i][1] + sorted_bounding_rect[i][3]
-        for j in range(i+1,len(sorted_bounding_rect)):
-            if sorted_bounding_rect[j][0] <= x_max + 3:
+        for j in range(i+1,len(sorted_bounding_rect)+1):
+            if j < len(sorted_bounding_rect) and sorted_bounding_rect[j][0] <= x_max + 3:
                 x_max = max(x_max,sorted_bounding_rect[j][0]+sorted_bounding_rect[j][2])
                 y_max = max(y_max,sorted_bounding_rect[j][1]+sorted_bounding_rect[j][3])
                 y_min = min(y_min,sorted_bounding_rect[j][1])
@@ -126,10 +126,8 @@ def segment_symbols(img, width = 16, height = 32):
                 i = j-1
                 new_sorted_bounding_rect.append((x_min,x_max,y_min,y_max))
                 break
-        if i == len(sorted_bounding_rect)-1 and sorted_bounding_rect[i][0] > new_sorted_bounding_rect[-1][0] + new_sorted_bounding_rect[-1][2] + 3:
-            new_sorted_bounding_rect.append((x_min,x_max,y_min,y_max))
-            break
         i +=1
+
     #Segment every Symbol and Put it as an image in the contours array
     contours_array = []
     borders = []
